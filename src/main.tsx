c0907812d0d4996014028edccfff17f6119bae6a
nikeshh03 <nikeshmpatil248@gmail.com>
2024-12-26 12:27:23 +0530

    final

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './lib/auth/AuthContext';
 import { ThemeProvider } from './lib/theme/ThemeContext';
+import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 
 const container = document.getElementById('root');
@@ -10,9 +11,11 @@ if (!container) throw new Error('Root element not found');
 const root = createRoot(container);
 
 root.render(
+  
   <React.StrictMode>
     <ThemeProvider>
       <AuthProvider>
+      <Analytics/>
         <App />
       </AuthProvider>
     </ThemeProvider>
